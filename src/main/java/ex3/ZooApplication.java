51d41285242ec5bf74f848c774c7e3ba2369de95
hmerciol <li.henri@merciol.fr>
2018-01-03 10:07:49 +0100

    lion et requin végétariens soignés ;) (ex 3)

--- src/main/java/ex3/ZooApplication.java
+++ src/main/java/ex3/ZooApplication.java
@@ -13,9 +13,9 @@ public class ZooApplication {
 
 		zoo.addAnimal(new Animal("Gazelle", AnimalType.MAMMIFERE, AnimalComportement.HERBIVORE));
 		zoo.addAnimal(new Animal("Zèbre", AnimalType.MAMMIFERE, AnimalComportement.HERBIVORE));
-		zoo.addAnimal(new Animal("Lion", AnimalType.MAMMIFERE, AnimalComportement.HERBIVORE));
+		zoo.addAnimal(new Animal("Lion", AnimalType.MAMMIFERE, AnimalComportement.CARNIVORE));
 		zoo.addAnimal(new Animal("Panthère", AnimalType.MAMMIFERE, AnimalComportement.CARNIVORE));
-		zoo.addAnimal(new Animal("Requin blanc", AnimalType.POISSON, AnimalComportement.HERBIVORE));
+		zoo.addAnimal(new Animal("Requin blanc", AnimalType.POISSON, AnimalComportement.CARNIVORE));
 		zoo.addAnimal(new Animal("Truite dorée", AnimalType.POISSON, AnimalComportement.HERBIVORE));
 		zoo.addAnimal(new Animal("Boa constrictor", AnimalType.REPTILE, AnimalComportement.CARNIVORE));
 		zoo.addAnimal(new Animal("Python", AnimalType.REPTILE, AnimalComportement.CARNIVORE));
